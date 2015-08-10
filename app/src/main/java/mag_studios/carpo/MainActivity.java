ac82c58f1e144b9c4a97fcaf7cd9fd0a86d4e450
Michael Grix <grixmich@gmail.com>
2015-08-10 16:01:24 -0400

    10-Aug-15 Combined Login button to login page

--- app/src/main/java/mag_studios/carpo/MainActivity.java
+++ app/src/main/java/mag_studios/carpo/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +36,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void LoginActivity(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
